2780d50a9a114fd365f62d9a6b0dfd311eec70cb
Alek Denslow <alekdenslow@gmail.com>
2018-06-13 21:18:33 -0700

    test

---
+++ FizzBuzz.java
@@ -0,0 +1,28 @@
+public class FizzBuzz {
+
+	public static void main(String[] args){
+		Sound[] sounds = {new Sound(3, "Fizz"), new Sound(5, "Buzz"), new Sound(7, "Baxx")};
+	for (int i = 1; i <= 20; i++){
+		Stringbuilder sb = new StringBuilder();
+		for (Sound sound : sounds) {
+			sb.append(sound.generate(i));
+		}
+		System.out.println(sb.length() == 0 ? i : sb.toString());
+	}
+}
+
+	private static class Sound {
+		private final int trigger;
+		private final String onomatopoeia;
+
+		public Sound(int trigger, String onomatopoeia) {
+			this.trigger = trigger;
+			this.onomatopoeia = onomatopeia;
+		}
+
+		public String generate(int i) {
+			return i % trigger == 0 ? onomatopoeia : "";
+		}
+	}
+	// this is a comment
+}
